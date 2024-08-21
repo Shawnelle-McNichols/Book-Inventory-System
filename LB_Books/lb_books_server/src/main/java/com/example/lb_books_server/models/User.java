4c92cf4ef11a69d7b4b5523345381ab88a24c43c
Shawnelle-McNichols <shawnellem101@gmail.com>
2024-08-21 12:12:32 -0400

    ix

--- LB_Books/lb_books_server/src/main/java/com/example/lb_books_server/models/User.java
+++
@@ -1,5 +0,0 @@
-package com.example.lb_books_server.models;
-
-public class User {
-    
-}
--- LB_Books/lb_books_server/src/main/java/com/example/lb_books_server/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.lb_books_server.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.lb_books_server.models.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer>{
-    
-}
